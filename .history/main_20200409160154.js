43e056825e43c3bc54caf4557ea798f4f3a30841
Kacper <kacper.s66@gmail.com>
2020-04-09 16:11:30 +0200

    afternon save

---
+++ .history/main_20200409160154.js
@@ -0,0 +1,80 @@
+// SLIDER
+var responsiveSlider = function() {
+
+    var slider = document.getElementById("slider");
+    var sliderWidth = slider.offsetWidth;
+    var slideList = document.getElementById("slideWrap");
+    var count = 1;
+    var items = slideList.querySelectorAll("li").length;
+    var prev = document.getElementById("prev");
+    var next = document.getElementById("next");
+
+    window.addEventListener('resize', function() {
+      sliderWidth = slider.offsetWidth;
+    });
+
+    var prevSlide = function() {
+      if(count > 1) {
+        count = count - 2;
+        slideList.style.left = "-" + count * sliderWidth + "px";
+        count++;
+      }
+      else if(count = 1) {
+        count = items - 1;
+        slideList.style.left = "-" + count * sliderWidth + "px";
+        count++;
+      }
+    };
+
+    var nextSlide = function() {
+      if(count < items) {
+        slideList.style.left = "-" + count * sliderWidth + "px";
+        count++;
+      }
+      else if(count = items) {
+        slideList.style.left = "0px";
+        count = 1;
+      }
+    };
+
+    next.addEventListener("click", function() {
+      nextSlide();
+    });
+
+    prev.addEventListener("click", function() {
+      prevSlide();
+    });
+
+    setInterval(function() {
+      nextSlide()
+    }, 10000);
+
+  };
+
+  window.onload = function() {
+    responsiveSlider();
+  }
+
+// Navigation fallow scroll
+
+const header = document.querySelector(".navigation");
+const sectionOne = document.querySelector(".section__slider");
+
+const sectionOneOptions = {
+  rootMargin: "-200px 0px 0px 0px"
+};
+
+const sectionOneObserver = new IntersectionObserver (function(
+  entries
+) {
+  entries.forEach(entry => {
+    if (!entry.isIntersecting) {
+      header.classList.add("nav-scrolled");
+    } else {
+      header.classList.remove("nav-scrolled");
+    }
+  });
+},
+sectionOneOptions);
+
+sectionOneObserver.observe(sectionOne);
